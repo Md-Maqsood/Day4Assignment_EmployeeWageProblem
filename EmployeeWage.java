9ee6de69ac92bfd4ba6e453b69e89f61711b39f9
maqsood <maqsoodhamd37@gmail.com>
2020-09-30 12:06:32 +0530

    [maqsood] update solved uc3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,15 @@
 public class EmployeeWage{
-	static final int FULL_TIME=1;
+	static final int PART_TIME=1;
+	static final int FULL_TIME=2;
 	static final int WAGE_PER_HOUR=20;
 	public static void main(String[] args){
 		int empHrs=0;
 		int empWage=0;
-		int empcheck=(int)(Math.floor(Math.random()*10)%2);
+		int empcheck=(int)(Math.floor(Math.random()*10)%3);
 		if (empcheck==FULL_TIME)
 			empHrs=8;
+		else if(empcheck==PART_TIME)
+			empHrs=4;
 		else
 			empHrs=0;
 		empWage=empHrs*WAGE_PER_HOUR;
