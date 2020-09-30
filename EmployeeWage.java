63420c74757bd43cc03caf40d369fd04aa4dca2a
maqsood <maqsoodhamd37@gmail.com>
2020-09-30 11:49:01 +0530

    [maqsood] update solved uc1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,10 @@
 public class EmployeeWage{
 	static final int FULL_TIME=1;
 	public static void main(String[] args){
-		System.out.println("Welcome to Employee Wage Computation Program");
+		int empcheck=(int)(Math.floor(Math.random()*10)%2);
+		if (empcheck==FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 }
