f6022f36462adb69dd8bb33250470becd5279228
maqsood <maqsoodhamd37@gmail.com>
2020-09-30 12:10:42 +0530

    [maqsood] update solved uc5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,21 +2,29 @@ public class EmployeeWage{
 	static final int PART_TIME=1;
 	static final int FULL_TIME=2;
 	static final int WAGE_PER_HOUR=20;
+	static final int WORKING_DAYS_IN_MONTH=20;
 	public static void main(String[] args){
-		int empHrs=0;
-		int empWage=0;
-		int empcheck=(int)(Math.floor(Math.random()*10)%3);
-		switch(empcheck){
-			case FULL_TIME:
-				empHrs=8;
-				break;
-			case PART_TIME:
-				empHrs=4;
-				break;
-			default:
-				empHrs=0;
+		int totalEmpWage=0;
+		for(int days=1;days<=WORKING_DAYS_IN_MONTH;days++){
+			int empHrs=0;
+			int empWage=0;
+			int empcheck=(int)(Math.floor(Math.random()*10)%3);
+			switch(empcheck){
+				case FULL_TIME:
+					empHrs=8;
+					break;
+				case PART_TIME:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+			}
+			empWage=empHrs*WAGE_PER_HOUR;
+			totalEmpWage+=empWage;
+			System.out.println("Employee Wage: "+empWage);
 		}
-		empWage=empHrs*WAGE_PER_HOUR;
-		System.out.println("Employee Wage: "+empWage);
+		System.out.println("Tolal Employee Wage: "+totalEmpWage);
 	}
 }
+	
+
