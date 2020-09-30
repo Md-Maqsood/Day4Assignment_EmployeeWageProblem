f2bcea1100384cea7b617e7687bfc3845d3deb05
maqsood <maqsoodhamd37@gmail.com>
2020-09-30 12:03:43 +0530

    [maqsood] update solved uc2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,15 @@
 public class EmployeeWage{
 	static final int FULL_TIME=1;
+	static final int WAGE_PER_HOUR=20;
 	public static void main(String[] args){
+		int empHrs=0;
+		int empWage=0;
 		int empcheck=(int)(Math.floor(Math.random()*10)%2);
 		if (empcheck==FULL_TIME)
-			System.out.println("Employee is present");
+			empHrs=8;
 		else
-			System.out.println("Employee is absent");
+			empHrs=0;
+		empWage=empHrs*WAGE_PER_HOUR;
+		System.out.println("Employee Wage: "+empWage);
 	}
 }
